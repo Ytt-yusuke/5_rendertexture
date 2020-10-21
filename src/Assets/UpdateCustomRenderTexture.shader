21d4e9d78f90570bec720d2915212ec568ec97fb
Yttlium <yusuke19991011@gmail.com>
2020-10-21 21:53:07 +0900

    課題提出

---
+++ src/Assets/UpdateCustomRenderTexture.shader
@@ -0,0 +1,24 @@
+﻿Shader "Unlit/UpdateCustomRenderTexture"
+{
+    SubShader
+    {
+        Lighting Off
+
+        Pass
+        {
+            CGPROGRAM
+
+            #include "UnityCustomRenderTexture.cginc"
+
+            #pragma vertex CustomRenderTextureVertexShader
+            #pragma fragment frag
+            #pragma target 3.0
+
+            float4 frag(v2f_customrendertexture i) :SV_Target
+            {
+                return float4 (frac(-i.direction + _Time), 1);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ src/Assets/new Camera/TurnCameraMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnCameraMover : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetModel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(targetModel.transform.position.x + 3.0f * Mathf.Sin(Time.time), 2.0f, targetModel.transform.position.z +  3.0f * Mathf.Cos(Time.time));
+
+        this.transform.LookAt(targetModel.transform.position);
+    }
+}
